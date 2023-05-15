6d489afd0db552f1b2f1ce6206960179bc271633
unknown <lakshmisriramadari1427@gmail.com>
2023-05-15 20:00:33 +0530

    first react project

---
+++ src/components/footer.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Footer(){
+    const curYear=new Date().getFullYear();
+    return <footer>
+        <p>Copyright © {curYear}</p>
+    </footer>
+}
+export default Footer;
---
+++ src/components/note.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Note(){
+    return <div className="note">
+    <h1 >title</h1>
+        <p >Content</p>
+    </div>
+}
+export default Note;
