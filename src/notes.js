be504c12716790a087574a60131d5062f76190c7
unknown <lakshmisriramadari1427@gmail.com>
2023-05-16 21:57:13 +0530

    added newnotes

---
+++ src/notes.js
@@ -0,0 +1,13 @@
+const Noteslist=[
+    {
+        id:1,
+        title:"FIRST DAY",
+        content:"first description"
+    },
+    {
+        id:2,
+        title:"second DAY",
+        content:"second description"
+    }
+]
+export default Noteslist; 
