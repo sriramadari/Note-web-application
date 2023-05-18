17f1f7e6c4df0c5f25f10aae91d1246a419e8fd2
unknown <lakshmisriramadari1427@gmail.com>
2023-05-18 23:17:46 +0530

    few more changes

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,38 @@
-import React from "react"
+import React, { useState } from "react"
 import Header from "./header"
 import Footer from "./footer"
 import Note from "./note"
+import CreateArea from "./CreateArea"
 function App(){
+    const [notes,setnotes]=useState([]);
+
+  function addNote(note){
+    setnotes(prevValue=>{
+      return [...prevValue,note];
+    })
+  }
+function deleteNote(id){
+setnotes(prevValue=>{
+ return prevValue.filter((noteitem,index)=>{
+    return index!==id;
+  });
+});
+}
     return <div>
     <Header/>
-    <Note/>
+    <CreateArea
+    onAdd={addNote}
+    />
+  { notes.map((eachNote,index)=>{
+    return (
+      <Note
+      id={index}
+        key={index}
+        title={eachNote.title}
+        desc={eachNote.content}
+        onDelete={deleteNote}
+      />  );
+  })}
     <Footer/>
     </div>
 }
--- src/components/note.jsx
+++ src/components/note.jsx
@@ -1,9 +1,15 @@
 import React from "react"
 
-function Note(){
+function Note(props){
+
+function handleClick(){
+props.onDelete(props.id);
+}
+
     return <div className="note">
-    <h1 >title</h1>
-        <p >Content</p>
+    <h1 >{props.title}</h1>
+        <p >{props.desc}</p>
+       <button onClick={handleClick}>DELETE</button>
     </div>
 }
 export default Note;
